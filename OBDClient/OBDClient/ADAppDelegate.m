639b4324306622f665d944c9893b44bb0234167f
humengchi <1099285805@qq.com>
2015-10-26 18:21:08 +0800

    sss

--- OBDClient/OBDClient/ADAppDelegate.m
+++ OBDClient/OBDClient/ADAppDelegate.m
@@ -3,7 +3,7 @@
 //  OBDClient
 //
 //  Created by 胡梦驰 on 15/9/1.
-//  Copyright (c) 2015年 胡梦驰. All rights reserved.
+//  Copyright (c) 2015年 胡梦驰``. All rights reserved.
 //
 
 #import "ADAppDelegate.h"
